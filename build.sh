d7ff68dba5331d97a3cbc524c8ea68abc9fd1f4d
陈佳慧 <jahv@oakhit.com>
2020-06-09 14:51:15 +0800

    将token固定在docker容器中，不再由群晖提供参数

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
+[ ! "$1" ] && exit 1
 docker build -t dingding-msg-virtual-service .
-docker run -d -p 6450:6450 --name dingding-msg-virtual-service --restart always dingding-msg-virtual-service
+docker stop -t 0 dingding-msg-virtual-service||true
+docker rm dingding-msg-virtual-service||true
+docker run -d -p 6450:6450 --name dingding-msg-virtual-service -e ACCESS_TOKEN=$1 --restart always dingding-msg-virtual-service
+docker logs -f dingding-msg-virtual-service
--- index.js
+++ index.js
@@ -1,60 +1,88 @@
-const http = require('http');
-const https = require('https');
+const http = require("http");
+const qs = require("querystring");
+const https = require("https");
 
-const server = http.createServer(function (req, res) {
-    if (req.url === '/send') {
-        const data = [];
-        req.on('data', function (d) {
-            data.push(d);
-        });
-        req.on('end', function () {
-            const message = Buffer.concat(data).toString();
-            try {
-                const data = JSON.parse(message);
-                const sendData = JSON.stringify({
-                    msgtype: "text",
-                    text: {
-                        content: data.message
-                    }
-                });
-                const result = https.request(`https://oapi.dingtalk.com/robot/send?access_token=${data.api_id}`, {
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }, function (res1) {
-                    const resultDataBuffer = [];
-                    res1.on("data", function (d) {
-                        resultDataBuffer.push(d);
-                    });
-                    res1.on("end", function () {
-                        try {
-                            const resultData = JSON.parse(Buffer.concat(resultDataBuffer).toString());
-                            if (resultData.errcode === 0) {
-                                res.writeHead(200);
-                                res.end();
-                            }
-                        } catch (e) {
-                            console.error(e);
-                        }
-                    });
-                    res1.on("error", function (e) {
-                        console.error(e);
-                    });
-                });
-                result.on('error', function (e) {
-                    console.error(e);
-                });
-                result.write(sendData);
-                result.end();
-            } catch (e) {
-                console.error(e);
+const access_token = process.env.ACCESS_TOKEN || process.argv[2];
+
+if (!access_token || access_token.length !== 64) {
+    throw new Error("请设置access_token，可以设置环境变量ACCESS_TOKEN或使用node index.js <access_token>");
+}
+
+function streamToBuffer(stream) {
+    return new Promise((resolve, reject) => {
+        let buffers = [];
+        stream.on("error", reject);
+        stream.on("data", (data) => buffers.push(data));
+        stream.on("end", () => resolve(Buffer.concat(buffers)));
+    });
+}
+
+const server = http.createServer(async function (req, res) {
+    if (req.url.startsWith("/send")) {
+        try {
+            let data;
+            if (req.method === "GET") {
+                data = qs.parse(req.url.substr(req.url.indexOf("?") + 1));
+            } else if (req.method === "POST") {
+                data = await streamToBuffer(req);
+                data = JSON.parse(data);
+            } else {
+                console.error("unsupported method");
             }
-        });
-        req.on('error', function (e) {
+            const sendData = JSON.stringify({
+                msgtype: "text",
+                text: {
+                    content: data.message,
+                },
+            });
+            console.log(sendData);
+            const sendOption = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            };
+            const result = https.request(`https://oapi.dingtalk.com/robot/send?access_token=${access_token}`, sendOption, function (res1) {
+                const resultDataBuffer = [];
+                res1.on("data", function (d) {
+                    resultDataBuffer.push(d);
+                });
+                res1.on("end", function () {
+                    try {
+                        const resultData = JSON.parse(Buffer.concat(resultDataBuffer).toString());
+                        if (resultData.errcode === 0) {
+                            res.writeHead(200);
+                            console.log("send success!");
+                            res.end();
+                        } else {
+                            console.log("send error!", resultData);
+                            res.end();
+                        }
+                    } catch (e) {
+                        console.error(e);
+                        res.end();
+                    }
+                });
+                res1.on("error", function (e) {
+                    console.error(e);
+                    res.end();
+                });
+            });
+            result.on("error", function (e) {
+                console.error(e);
+            });
+            result.write(sendData);
+            result.end();
+        } catch (e) {
             console.error(e);
-        })
+            res.end();
+        }
+    } else {
+        console.log("no api", req.url);
+        res.end();
     }
 });
 
-server.listen(6450);
+server.listen(6450, function () {
+    console.log("server run on", server.address().port);
+});
